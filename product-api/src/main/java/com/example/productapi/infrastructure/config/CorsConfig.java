bca3d46384e471f1bd25f657980498813fe623c9
sconde2025 <souleymaneconde2020@gmail.com>
2025-11-09 12:35:23 +0100

    [PRODUCT-API]Add Cors Config

--- product-api/src/main/java/com/example/productapi/infrastructure/config/CorsConfig.java
+++ product-api/src/main/java/com/example/productapi/infrastructure/config/CorsConfig.java
@@ -1,4 +1,24 @@
 package com.example.productapi.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")   // apply to your API paths
+                        .allowedOrigins("http://localhost:4200") // frontend origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
+
